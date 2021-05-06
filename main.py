0906b5c6c6c1003d7c540b8440a73e9fcc5e46bf
Lena Cabrera <lena.m.cabrera@gmail.com>
2021-05-07 00:52:15 +0200

    Added preprocessing for color and spatial features.

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
+import os
+import scipy.io
+import matplotlib.pyplot as plt
+from enum import Enum
+import numpy as np
+
+
+class Features(Enum):
+    color = 0
+    color_spatial = 1
+
+
+
+def load_data(path):
+    """ Loads the data """
+    data = scipy.io.loadmat(path)["data"]
+    return data
+
+
+def load_images(dir, filenames):
+    images = []
+    for filename in filenames:
+        images.append(plt.imread(dir + filename, 0))
+    return images
+
+
+def reshape_img(img, features):
+
+    if features == Features.color:
+        image = img.reshape((img.shape[2], img.shape[0] * img.shape[1]))
+    if features == Features.color_spatial:
+        x = np.array(list(range(img.shape[0])))
+        y = np.array(list(range(img.shape[1])))
+        coordinates = np.array(np.meshgrid(x, y))
+        image = np.append(img, coordinates.T, axis=2)
+    return image
 
 
 def find_peak(data, idx, r):
@@ -128,3 +164,12 @@ def image_segmentation(img, r):
     # color.rgb2lab(img)
     # then convert the resulting cluster centers back to RGB using color.lab2rgb()
     pass
+
+
+if __name__ == '__main__':
+    # data = load_data("data/pts.mat")
+    images = load_images(dir='./img/', filenames=["181091.jpg", "368078.jpg"])
+    img = images[0]
+    img = reshape_img(img, Features.color_spatial)
+    print(img.shape, img[0,0])
+
